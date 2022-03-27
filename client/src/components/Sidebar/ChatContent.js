7cb4d7005470cb620e66f7eb471bf55808bba338
Dave Quinn <dquinn8@cogeco.ca>
2022-03-27 13:26:58 -0400

    unread messages in the ChatContent component now display as bold, black text

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,22 +1,27 @@
-import React from "react";
-import { Box, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between',
     marginLeft: 20,
     flexGrow: 1,
   },
   username: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     letterSpacing: -0.2,
   },
   previewText: {
     fontSize: 12,
-    color: "#9CADC8",
+    color: '#9CADC8',
     letterSpacing: -0.17,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    letterSpacing: -0.2,
+    color: 'black',
   },
 }));
 
@@ -25,6 +30,8 @@ const ChatContent = ({ conversation }) => {
 
   const { otherUser } = conversation;
   const latestMessageText = conversation.id && conversation.latestMessageText;
+  const latestMessageRead =
+    conversation.messages[conversation.messages.length - 1].readState;
 
   return (
     <Box className={classes.root}>
@@ -32,7 +39,11 @@ const ChatContent = ({ conversation }) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        <Typography className={classes.previewText}>
+        <Typography
+          className={`${classes.previewText} ${
+            latestMessageRead ? '' : `${classes.boldText}`
+          }`}
+        >
           {latestMessageText}
         </Typography>
       </Box>

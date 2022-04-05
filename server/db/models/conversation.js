41c3939a3f7e90d426bed276a7adc50c4cb5411c
Dave Quinn <dquinn8@cogeco.ca>
2022-04-05 19:33:05 -0400

    re linted server/db/models/conversation.js and adjusted serverside prettierrrc.json config

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,21 +1,22 @@
-const { Op } = require('sequelize');
-const db = require('../db');
-const Message = require('./message');
+const { Op } = require("sequelize");
+const db = require("../db");
+const Message = require("./message");
 
-const Conversation = db.define('conversation', {});
-const UserConvos = db.define('convos', {}, { timestamps: false });
+const Conversation = db.define("conversation", {});
+const UserConvos = db.define("convos", {}, { timestamps: false });
 
 // find conversation given two user Ids
+
 Conversation.findConversation = async function (user1Id, user2Id) {
   const conversation = await Conversation.findOne({
     where: {
       user1Id: {
-        [Op.or]: [user1Id, user2Id],
+        [Op.or]: [user1Id, user2Id]
       },
       user2Id: {
-        [Op.or]: [user1Id, user2Id],
-      },
-    },
+        [Op.or]: [user1Id, user2Id]
+      }
+    }
   });
 
   // return conversation or null if it doesn't exist
@@ -25,12 +26,12 @@ Conversation.findConversation = async function (user1Id, user2Id) {
 Conversation.userParticipates = async function (conversationId, userId) {
   const conversation = await Conversation.findOne({
     where: {
-      [Op.or]: [{ user1Id: userId }, { user2Id: userId }],
-      id: conversationId,
-    },
+      [Op.or]: [{user1Id:userId}, {user2Id:userId}],
+      id: conversationId
+    }
   });
 
   return !!conversation;
 };
 
-module.exports = { Conversation, UserConvos };
+module.exports = Conversation;

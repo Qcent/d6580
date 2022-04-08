119c947502358bb2732bbf15dbbd0d9ab772b4cc
Dave Quinn <dquinn8@cogeco.ca>
2022-04-07 23:23:49 -0400

    relinted db/models/index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,16 +1,16 @@
-const { Conversation, UserConvos } = require('./conversation');
-const User = require('./user');
-const Message = require('./message');
+const { Conversation, UserConvos } = require("./conversation");
+const User = require("./user");
+const Message = require("./message");
 
 // associations
 
 User.belongsToMany(Conversation, { through: UserConvos });
-Conversation.belongsToMany(User, { as: 'members', through: UserConvos });
+Conversation.belongsToMany(User, { as: "members", through: UserConvos });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
 module.exports = {
   User,
   Conversation,
-  Message,
+  Message
 };
